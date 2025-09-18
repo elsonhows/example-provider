44ec95d51c439fed09969114a4ae528ffdce7672
Elson Ho <36079841+elsonhows@users.noreply.github.com>
2025-09-18 20:01:37 +0800

    change phone value fron num to string

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,9 +4,9 @@ class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", 123, "green")],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", 456, "blue")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", 789, "yellow")],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", '123', "green")],
+            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", '456', "blue")],
+            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", '789', "yellow")],
         ]);
     }
 
